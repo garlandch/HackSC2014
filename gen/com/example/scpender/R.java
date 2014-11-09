4abc7c6f71150393fb297c92f7be8e89fb053824
Garland106 <garlandc@usc.edu>
2014-11-09 05:05:47 -0800

    BadAssets Listview

--- gen/com/example/scpender/R.java
+++ gen/com/example/scpender/R.java
@@ -989,6 +989,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f02005c;
     }
     public static final class id {
+        public static final int BadAssetsListView=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1000,16 +1001,17 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int archivestab=0x7f050040;
-        public static final int badassetstab=0x7f05003f;
+        public static final int archivestab=0x7f050041;
+        public static final int badassetstab=0x7f050040;
         public static final int beginning=0x7f050011;
-        public static final int buttonbar=0x7f05003d;
+        public static final int buttonbar=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
+        public static final int dateTextview=0x7f050043;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1029,6 +1031,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int priceTextview=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1047,11 +1050,12 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
+        public static final int storeTextview=0x7f050044;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int totalstab=0x7f05003e;
+        public static final int totalstab=0x7f05003f;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1110,6 +1114,7 @@ containing a value of this type.
         public static final int activity_main=0x7f030019;
         public static final int buttonbar=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int transaction_list_item=0x7f03001c;
     }
     public static final class menu {
         public static final int archives_page=0x7f0c0000;
--- src/com/scpender/BadAssetsPage.java
+++ src/com/scpender/BadAssetsPage.java
@@ -1,17 +1,99 @@
 package com.scpender;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.scpender.R;
 
-public class BadAssetsPage extends ActionBarActivity {
-
+public class BadAssetsPage extends ActionBarActivity 
+{
+	private ListView badassetsLV;
+	private BadAssetListAdapter listAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bad_assets_page);
 		TabUtils.activateTab(this, R.id.badassetstab);
+		
+		//Hacked in Items
+		List<String> myTransactions = new ArrayList<String>();
+		myTransactions.add("first");
+		myTransactions.add("first");
+		myTransactions.add("first");
+		
+		badassetsLV = (ListView) findViewById(R.id.BadAssetsListView);
+		listAdapter = new BadAssetListAdapter(getApplicationContext(), myTransactions);
+		badassetsLV.setAdapter(listAdapter);
+	}
+	
+	private class BadAssetListAdapter extends ArrayAdapter<String>
+	{
+		private List<String> transactions;
+		private Context mContext = null;
+		private LayoutInflater inflater = null;
+		
+		private class ViewHolder 
+		{
+            TextView date;
+            TextView store;
+            TextView price;
+        }
+		
+		public BadAssetListAdapter(Context context, List<String> items) 
+		{
+			super(context, R.layout.transaction_list_item, items);
+			this.transactions = items;
+			this.mContext = context;
+			this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public int getCount() 
+		{
+			return transactions.size();
+		}
+
+		@Override
+		public String getItem(int item) 
+		{
+			return transactions.get(item);
+		}
+
+		@Override
+		public long getItemId(int id) 
+		{
+			return id;
+		}
+
+		@Override
+		public View getView(int position, View view, ViewGroup parent) 
+		{
+			ViewHolder viewholder = new ViewHolder();
+			view = inflater.inflate(R.layout.transaction_list_item, parent, false);
+			
+			viewholder.date = (TextView) view.findViewById(R.id.dateTextview);
+			viewholder.price = (TextView) view.findViewById(R.id.priceTextview);
+			viewholder.store = (TextView) view.findViewById(R.id.storeTextview);
+			
+			viewholder.date.setText("Nov 6, 2014");
+			viewholder.price.setText("$69.99");
+			viewholder.store.setText("Umami Burger");
+			
+			view.setTag(viewholder);
+			return view;
+		}
+		
 	}
 }
